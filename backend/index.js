ebe7b4943608f2e28f89f2b32f82b3aef2107b80
TIMace <timos@email.com>
2018-02-21 18:02:33 +0200

    packages.json: Added express.js depencency and body-parser index.js: added user and provider GET and POST functions. Server setup.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 'use strict'
 
-const models = require('./models')
+const express = require('express')
+    , bodyParser = require('body-parser')
+    , app = express()
+
+    , fs = require('fs')
+    , config = require('./config').config
+
+    , models = require('./models')
     , sequelize = models.sequelize
     , User = models.User
     , Provider = models.Provider
@@ -10,4 +17,116 @@ const models = require('./models')
     , EventCategory = models.EventCategory
 
 
+// Middleware
+// Support JSON encoded bodies
+// Support extended URL encoded bodies
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded( { extended : true } ))
+
+
+/**
+ * * * * * * * * * * * * * * * * * * * * ROUTES * * * * * * * * * * * * * * * * * * * *
+ *
+ * - DBSchema
+ *      - GET (Get all Schemas in DB)
+ *
+ * - USER
+ *      - GET (Get all data for respective user. Use for login)
+ *      - POST (Create user giving required parameters)
+ *      - PUT (Update user giving required parameters)
+ *
+ * - Provider
+ *      - GET (Get all data for respective provider. Use for login)
+ *      - POST (Create provider giving required parameters)
+ *      - PUT (Update provider giving required parameters)
+ * * * * * * * * * * * * * * * * * * * ** * * * * * * * * * * * * * * * * * * * * * * */
+
+app.get('/DBSchema', (req, res) => {
+    res.json({
+        User : Object.keys(User.attributes),
+        Provider : Object.keys(Provider.attributes),
+        Transaction : Object.keys(Transaction.attributes),
+        Evnt : Object.keys(Evnt.attributes),
+        Category : Object.keys(Category.attributes),
+        EventCategory : Object.keys(EventCategory.attributes),
+    })
+})
+
+
+/**
+ * USER
+ */
+
+app.get('/user/:username/:password', (req, res) => {
+    User.findOne( { where : { username : req.params.username, user_password : req.params.password } } ).then((user) => {
+        res.json(user)
+    })
+})
+
+app.post('/user', (req, res) => {
+    const uname = req.body.username
+        , passwd = req.body.password
+        , email = req.body.email
+        , fname = req.body.fname
+        , lname = req.body.lname
+        , addr = req.body.address
+        , phnum = req.body.phone_num
+
+    User
+        .findOrCreate( {
+            where : { username : uname, user_password : passwd },
+            defaults : { user_email : email, user_first_name : fname, user_last_name : lname, user_address : addr, user_phone_num : phnum } } )
+        .spread((user, created) => {
+            if (created)
+                res.json(user.get( {plain : true }))
+            else
+                res.json(null)
+        })
+})
+
+
+/**
+ * PROVIDER
+ */
+
+app.get('/provider/:username/:password', (req, res) => {
+    Provider.findOne( { where : { username : req.params.username, user_password : req.params.password } } ).then((provider) => {
+        res.json(provider)
+    })
+})
+
+app.post('/provider', (req, res) => {
+    const uname = req.body.username
+        , passwd = req.body.password
+        , email = req.body.email
+        , fname = req.body.fname
+        , lname = req.body.lname
+        , cname = req.body.cname
+        , addr = req.body.address
+        , phnum = req.body.phone_num
+        , ssn = req.body.ssn
+        , baccount = req.body.baccount
+
+    Provider
+        .findOrCreate( {
+            where : { username : uname, provider_password : passwd },
+            defaults : { provider_email : email, provider_first_name : fname, provider_last_name : lname, provider_comp_name : cname,
+                provider_address : addr, provider_phone_num : phnum, provider_ssn : ssn, provider_bank_account : baccount } } )
+        .spread((provider, created) => {
+            if (created)
+                res.json(provider.get( {plain : true }))
+            else
+                res.json(null)
+        })
+})
+
+
+
 sequelize.sync()
+
+
+const server = app.listen(config.port, () => {
+    console.log(`Listening on ${server.address().address} : ${server.address().port}`)
+})
+
+
