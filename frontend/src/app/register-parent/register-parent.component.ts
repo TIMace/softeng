d1178bac02415c87884f8f69873e2e39c4757ae0
Maria <maria_6nhrak@hotmail.com>
2018-02-23 01:57:24 +0200

    Forms RegisterParent, RegisterProvider

--- frontend/src/app/register-parent/register-parent.component.ts
+++ frontend/src/app/register-parent/register-parent.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder, Validators, FormControl, AbstractControl } from '@angular/forms';
 
 
 @Component({
@@ -9,30 +9,46 @@ import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder, Validators, F
 })
 export class RegisterParentComponent implements OnInit {
 
-  formRegister: FormGroup;
+  formRegisterParent: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder
   ) {}
 
-  doRegister(){
-    console.log(event);
-    console.log(this.formRegister.value);
+  doRegisterProvider(){
+    console.log(this.formRegisterParent.value);
   }
 
   ngOnInit() {
+    this.formRegisterParent = this.formBuilder.group({
+      parent_name: this.parent_name,
+      parent_lastname: this.parent_lastname,
+      parent_location: this.parent_location,
+      parent_phone: this.parent_phone,
+      parent_username: this.parent_username,
+      parent_email: this.parent_email,
+      parent_password: this.parent_password,
+      parent_confirm_password: this.parent_confirm_password,
+    } , { validator: this.matchingPasswords('parent_password', 'parent_confirm_password')} );
+  }
 
-    this.formRegister = this.formBuilder.group({
-      name: ['Λεωνίδας', Validators.required],
-      lastname: ['Παπαντωνίου', Validators.required],
-      location: ['', Validators.required],
-      phone: ['', Validators.required],
-      username: ['Leo', Validators.required],
-      email: ['', [Validators.required, Validators.email]], //Validators.pattern('[a-z0-9.@]*')
-      password: ['', Validators.required],
-      confirm_password: ['', Validators.required],
-    });
+  parent_name = new FormControl("Λεωνίδας", Validators.required);
+  parent_lastname = new FormControl("Παπαντωνίου", Validators.required);
+  parent_location = new FormControl("", Validators.required);
+  parent_phone = new FormControl("", Validators.required);
+  parent_username = new FormControl("LeoPapa", Validators.required);
+  parent_email = new FormControl("", [Validators.required, Validators.email]);
+  parent_password = new FormControl("", Validators.required);
+  parent_confirm_password = new FormControl("", Validators.required);
 
+  matchingPasswords(passwordKey: string, passwordConfirmationKey: string) {
+    return (group: FormGroup) => {
+      let passwordInput = group.controls[passwordKey];
+      let passwordConfirmationInput = group.controls[passwordConfirmationKey];
+      if (passwordInput.value !== passwordConfirmationInput.value) {
+        return passwordConfirmationInput.setErrors({notEquivalent: true})
+      }
+    }
   }
 
 }
