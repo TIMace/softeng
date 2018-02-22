82da4f1f36508df8697d8b1d1269a7a030e6094e
Smitsos <dimitris_sma@hotmail.com>
2018-02-23 01:16:56 +0200

    Wallet

--- frontend/src/app/wallet/wallet.component.ts
+++ frontend/src/app/wallet/wallet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-wallet',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wallet.component.css']
 })
 export class WalletComponent implements OnInit {
+  
+  payment : number;
+  paymentBool = false;
+  formRegister: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.formRegister = this.formBuilder.group({
+      cardNumber: ['xxxx-xxxx-xxxx-xxxx', Validators.required],
+      password: ['', Validators.required],
+      amount: ['50€', Validators.required]
+      });
+  }
+  onPayment() {
+    this.payment = 0;
+    this.paymentBool = true;
   }
 
 }
