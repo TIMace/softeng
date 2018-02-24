fa8765ca9744155c336a15a681335bdb062598fe
Orestis Roditis <orestarod@gmail.com>
2018-02-24 17:16:34 +0200

    some changes, commiting to pull

--- frontend/src/app/categories.service.ts
+++ frontend/src/app/categories.service.ts
@@ -3,16 +3,25 @@ import { Category } from './category';
 import { CATEGORIES } from './mock-categories';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import {server_addr} from './server_addr'
+import {HttpClient} from '@angular/common/http';
+import {HttpErrorResponse} from '@angular/common/http';
+import { HttpParams, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http'; 
 
 @Injectable()
 export class CategoriesService {
 
   selectedCategories = [];
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
   
   getCategories(): Observable<Category[]> {
+    var req = this.httpClient.get(
+      `${server_addr}/category`
+    ).map((response: Response) => <Category[]>(response.json()))
     return of(CATEGORIES);
+    // return req;
   }
 
   getSelectedCategories(): Category[] {
