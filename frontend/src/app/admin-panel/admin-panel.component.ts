9b7dded74184e4c168a69ff2ee1b1203bf990bb4
SifuMaster <phivos93@yahoo.com>
2018-02-26 01:49:59 +0200

    admin panel pay events button added

--- frontend/src/app/admin-panel/admin-panel.component.ts
+++ frontend/src/app/admin-panel/admin-panel.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {HttpErrorResponse} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { HttpParams, HttpHeaders, HttpRequest } from '@angular/common/http';
-import { Http, Response, Headers, RequestOptions } from '@angular/http'; 
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { UserDetailsService } from '../user-details.service'
 
 
@@ -15,146 +15,179 @@ import { UserDetailsService } from '../user-details.service'
 })
 export class AdminPanelComponent implements OnInit {
 
-  username:String = '';
-  providerUsername:String = '';
-  radio:String = 'parent';
+  username: String = '';
+  providerUsername: String = '';
+  radio: String = 'parent';
 
-  constructor(private httpClient:HttpClient, private adminDetails:UserDetailsService) { }
-  onNameKeyUp(event:any) {
+  constructor(private httpClient: HttpClient, private adminDetails: UserDetailsService) { }
+  onNameKeyUp(event: any) {
     // console.log(event.target.value);
     this.username = event.target.value;
-      // console.log(this.radio);
+    // console.log(this.radio);
   }
 
-  onNameKeyUpProvider(event:any) {
+  onNameKeyUpProvider(event: any) {
     // console.log(event.target.value);
     this.providerUsername = event.target.value;
-      // console.log(this.radio);    
+    // console.log(this.radio);    
   }
 
   // adminUsername = this.adminDetails.userDetails.username;
   // adminPassword = this.adminDetails.userDetails.password;
-  
+
   //decision depends on radio button
-  lock(){
-    if (this.radio == 'parent'){this.lockUser(this.username)}
-    else{this.lockProvider(this.username)}
+  lock() {
+    if (this.radio == 'parent') { this.lockUser(this.username) }
+    else { this.lockProvider(this.username) }
   }
 
   // //decision depends on radio button
-  unlock(){
-    if (this.radio == 'parent'){this.unlockUser(this.username)}
-    else{this.unlockProvider(this.username)}
+  unlock() {
+    if (this.radio == 'parent') { this.unlockUser(this.username) }
+    else { this.unlockProvider(this.username) }
   }
 
 
-  lockUser(username:String) {
+  lockUser(username: String) {
     var userLock = new HttpParams()
-    .set('username', ''+this.adminDetails.userDetails.username)
-    .set('password', ''+this.adminDetails.userDetails.password)
-    .set('name', ''+this.username);
+      .set('username', '' + this.adminDetails.userDetails.username)
+      .set('password', '' + this.adminDetails.userDetails.password)
+      .set('name', '' + this.username);
     this.httpClient.post('http://snf-806935.vm.okeanos.grnet.gr:8888/admin/usr_deactivate',
       userLock.toString(),
       {
         headers: new HttpHeaders()
-        .set('Content-Type', 'application/x-www-form-urlencoded')
+          .set('Content-Type', 'application/x-www-form-urlencoded')
       }
     )
-    .subscribe(
-      (data:any) => {
-        alert("User Locked Succesfully");
-      },
-      (err: HttpErrorResponse) => {
-        if (err.error instanceof Error) {
-          console.log("Client-side error occured.");
-        } else {
-          console.log("Server-side error occured.");
+      .subscribe(
+        (data: any) => {
+          alert("User Locked Succesfully");
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log("Client-side error occured.");
+          } else {
+            console.log("Server-side error occured.");
+          }
         }
-      }
-    )
+      )
   }
 
-  lockProvider(username:String) {
+  lockProvider(username: String) {
     var providerLock = new HttpParams()
-    .set('username', ''+this.adminDetails.userDetails.username)
-    .set('password', ''+this.adminDetails.userDetails.password)
-    .set('name', ''+this.providerUsername);    
+      .set('username', '' + this.adminDetails.userDetails.username)
+      .set('password', '' + this.adminDetails.userDetails.password)
+      .set('name', '' + this.providerUsername);
     this.httpClient.put('http://snf-806935.vm.okeanos.grnet.gr:8888/admin/pr_deactivate',
       providerLock.toString(),
       {
         headers: new HttpHeaders()
-        .set('Content-Type', 'application/x-www-form-urlencoded')
+          .set('Content-Type', 'application/x-www-form-urlencoded')
       }
     )
-    .subscribe(
-      (data:any) => {
-        alert("Provider Locked Succesfully");
-      },
-      (err: HttpErrorResponse) => {
-        if (err.error instanceof Error) {
-          console.log("Client-side error occured.");
-        } else {
-          console.log("Server-side error occured.");
+      .subscribe(
+        (data: any) => {
+          alert("Provider Locked Succesfully");
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log("Client-side error occured.");
+          } else {
+            console.log("Server-side error occured.");
+          }
         }
-      }
-    )
+      )
   }
 
-  unlockUser(username:String) {
+  unlockUser(username: String) {
     var userUnLock = new HttpParams()
-    .set('username', ''+this.adminDetails.userDetails.username)
-    .set('password', ''+this.adminDetails.userDetails.password)
-    .set('name', ''+this.username);
+      .set('username', '' + this.adminDetails.userDetails.username)
+      .set('password', '' + this.adminDetails.userDetails.password)
+      .set('name', '' + this.username);
     this.httpClient.post('http://snf-806935.vm.okeanos.grnet.gr:8888/admin/usr_activate',
       userUnLock.toString(),
       {
         headers: new HttpHeaders()
-        .set('Content-Type', 'application/x-www-form-urlencoded')
+          .set('Content-Type', 'application/x-www-form-urlencoded')
       }
     )
-    .subscribe(
-      (data:any) => {
-        alert("User Unlocked Succesfully");
-      },
-      (err: HttpErrorResponse) => {
-        if (err.error instanceof Error) {
-          console.log("Client-side error occured.");
-        } else {
-          console.log("Server-side error occured.");
+      .subscribe(
+        (data: any) => {
+          alert("User Unlocked Succesfully");
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log("Client-side error occured.");
+          } else {
+            console.log("Server-side error occured.");
+          }
         }
-      }
-    )
+      )
   }
 
-  unlockProvider(username:String) {
+  unlockProvider(username: String) {
     var providerUnLock = new HttpParams()
-    .set('username', ''+this.adminDetails.userDetails.username)
-    .set('password', ''+this.adminDetails.userDetails.password)
-    .set('name', ''+this.username);
+      .set('username', '' + this.adminDetails.userDetails.username)
+      .set('password', '' + this.adminDetails.userDetails.password)
+      .set('name', '' + this.username);
     this.httpClient.post('http://snf-806935.vm.okeanos.grnet.gr:8888/admin/pr_activate',
       providerUnLock.toString(),
       {
         headers: new HttpHeaders()
-        .set('Content-Type', 'application/x-www-form-urlencoded')
+          .set('Content-Type', 'application/x-www-form-urlencoded')
       }
     )
-    .subscribe(
-      (data:any) => {
-        alert("Provider Unlocked Succesfully");
-      },
-      (err: HttpErrorResponse) => {
-        if (err.error instanceof Error) {
-          console.log("Client-side error occured.");
-        } else {
-          console.log("Server-side error occured.");
+      .subscribe(
+        (data: any) => {
+          alert("Provider Unlocked Succesfully");
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log("Client-side error occured.");
+          } else {
+            console.log("Server-side error occured.");
+          }
         }
-      }
-    )
+      )
   }
 
-  activateProvider(providerUsername:String) {
+  activateProvider(providerUsername: String) {
     this.radio = 'Provider';
     this.unlock();
+  }
+
+  eventId;
+  payEvent() {
+    var eventPayment = new HttpParams()
+      .set('username', '' + this.adminDetails.userDetails.username)
+      .set('password', '' + this.adminDetails.userDetails.password)
+      .set('ev_id', '' + this.eventId);
+    this.httpClient.post('http://snf-806935.vm.okeanos.grnet.gr:8888/admin/pay_event',
+      eventPayment.toString(),
+      {
+        headers: new HttpHeaders()
+          .set('Content-Type', 'application/x-www-form-urlencoded')
+      }
+    )
+      .subscribe(
+        (data: any) => {
+          console.log(data);
+          if (data === null) {
+            alert("Event is already paid")
+          }
+          else if (Number.isInteger(data)) {
+            alert("Event Paid Successfully");
+          }
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log("Client-side error occured.");
+          } else {
+            console.log("Server-side error occured.");
+          }
+        }
+      )
   }
 
   ngOnInit() {
