1e3039bc52e23a58dc655f8bace7cae969e9f9af
Orestis Roditis <orestarod@gmail.com>
2018-02-26 19:58:44 +0200

    added parentGetEventTickets

--- frontend/src/app/event.service.ts
+++ frontend/src/app/event.service.ts
@@ -155,6 +155,26 @@ export class EventService {
     return subject.asObservable();
   }
 
+  parentGetEventTickets(id){
+    var subject = new Subject();
+    var uname = this.userDetailsService.userDetails.username;
+    var passwd = this.userDetailsService.userDetails.password;
+
+    this.httpClient.get(
+      `${server_addr}/user/event/${uname}/${passwd}/${id}`,
+    )
+    .subscribe(
+      (data:any)=>
+      {
+        var res = [];
+        for(var i=0;i<data.lenght;i++){
+          res.push(data[i].transaction_id)
+        }
+        subject.next(res)
+      }
+    )
+  }
+
   buyEvent(id){
     var subject = new Subject();
     var userDetails = this.userDetailsService.getDetails()
@@ -357,6 +377,7 @@ export class EventService {
       res.name = server_event.event_name
       res.price = +server_event.event_price
       res.provider_id = +server_event.event_provider_id
+      res.img = "https://i.ytimg.com/vi/A4wP_VUPOAo/maxresdefault.jpg"
       final_res.push(res);
     }
     // console.log("Final Res:")
@@ -382,6 +403,7 @@ export class EventService {
       res.name = server_event.event_name
       res.price = +server_event.event_price
       res.provider_id = +server_event.event_provider_id
+      res.img = "https://i.ytimg.com/vi/A4wP_VUPOAo/maxresdefault.jpg"
       // final_res.push(res);
     // }
     // console.log("Final Res:")
