16c9995357f517011dbb9fac2e10efc61984af91
Orestis Roditis <orestarod@gmail.com>
2018-02-27 16:15:38 +0200

    fixed password reset

--- frontend/src/app/reset-password/reset-password.component.ts
+++ frontend/src/app/reset-password/reset-password.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {HttpErrorResponse} from '@angular/common/http';
+import { HttpParams, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http'; 
+import { server_addr} from '../server_addr'
 
 @Component({
   selector: 'app-reset-password',
@@ -10,11 +15,32 @@ export class ResetPasswordComponent implements OnInit {
 
   formReset: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,private httpClient:HttpClient) { }
 
   doReset(){
-    console.log(event);
+    // console.log(event);
     console.log(this.formReset.value);
+    this.httpClient.get(
+      `${server_addr}/user_reset/${this.formReset.value.email}`
+    ).subscribe(
+    (data:any)=>{
+      console.log(data)
+      if (data==null || data.hasOwnProperty("error")){
+        alert("Η επαναφορά κωδικού απέτυχε!")
+      }
+      else{
+        alert("Η επαναφορά κωδικού ήταν επιτυχής!")
+      }
+
+    },
+    (err: HttpErrorResponse) => {
+      if (err.error instanceof Error) {
+        console.log("Client-side error occured.");
+      } else {
+        console.log("Server-side error occured.");
+      }
+      // subject.next(false)2
+    })
   }
 
   ngOnInit() {
