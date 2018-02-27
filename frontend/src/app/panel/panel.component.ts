3ac34a9b2af98d57213be11f50a0fc3afae702b4
SifuMaster <phivos93@yahoo.com>
2018-02-27 04:15:11 +0200

    provider checout

--- frontend/src/app/panel/panel.component.ts
+++ frontend/src/app/panel/panel.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDetailsService, userDetailsObj } from '../user-details.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { HttpParams, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { server_addr } from '../server_addr'
+
 
 @Component({
   selector: 'app-panel',
@@ -9,27 +15,63 @@ import { UserDetailsService, userDetailsObj } from '../user-details.service';
 export class PanelComponent implements OnInit {
 
   constructor(
-    public userDetailsService: UserDetailsService
+    public userDetailsService: UserDetailsService,
+    private httpClient: HttpClient
   ) { }
-  
+
   show: number;
   show_more: boolean;
 
-  selected_personal_info(){
+  selected_personal_info() {
     this.show = 1;
   }
 
-  selected_present_events(){
+  selected_present_events() {
     this.show = 2;
   }
 
-  selected_past_events(){
+  selected_past_events() {
     this.show = 3;
   }
 
-  create_event(){
+  create_event() {
     this.show = 4;
   }
+
+  clearPoints() {
+    var usr = this.userDetailsService.userDetails.username;
+    var psw = this.userDetailsService.userDetails.password;
+    var eventPayment = new HttpParams()
+      .set('username', '' + usr)
+      .set('password', '' + psw);
+    this.httpClient.get(`${server_addr}/provider/credits/${usr}/${psw}`,
+      {
+        headers: new HttpHeaders()
+          .set('Content-Type', 'application/x-www-form-urlencoded')
+      }
+    )
+      .subscribe(
+        (data: any) => {
+          // console.log("AMOUNTTTT")
+          // console.log(this.eventId)
+          // console.log(data);
+          if (data == null) {
+            alert("Error")
+          }
+          else {
+            alert("Provider Paid Successfully");
+          }
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            console.log("Client-side error occured.");
+          } else {
+            console.log("Server-side error occured.");
+          }
+        }
+      )
+  }
+
 
   ngOnInit() {
     this.show = 1;
@@ -37,3 +79,4 @@ export class PanelComponent implements OnInit {
   }
 
 }
+
